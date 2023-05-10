515bb1a0a77ddfef3e3d2fbce67b4ccb6eb5f680
Mohamed Ali <Mohamedelmahi92@outlook.com>
2023-05-10 20:41:14 +0200

    Working on lab 2

---
+++ lab 2.R
@@ -0,0 +1,79 @@
+library(mvtnorm)
+library(readxl)
+
+#reading the files 
+df<-read_xlsx("Linkoping2022.xlsx")
+#Creating the covariate_time vaiable as (the number of days sinvce the beginning of the year / 365)  
+a<- df$datetime
+#begunning of the year
+b<- '2022-01-01'
+a<-format.POSIXlt(strptime(a,'%Y-%m-%d'))
+b<-format.POSIXlt(strptime(b,'%Y-%m-%d'))
+#time diff from the 
+x<-as.vector(difftime(a,b,units='days'))
+df$cov_tm<-x/365
+
+########
+###A####
+########
+#We assume to use a conjugate prior from the linear regression in Lec 5 , we have been given the prior hyperparamteres as follow:
+mu_0= as.matrix(c(0,100,-100),ncol=3)
+omega_0=0.01*diag(3)
+v_0=1
+segma2_0=1
+n= length(df$temp)
+ndraws=10
+
+# we have the joint prior for beta and segma2 defined as B|Segma2 follows N(muo,sigma2*omegao_inv) and Segma2 follows Inv-Chi(v0,sigma2_0)
+
+#First we draw our random samaple from inv-chi2 using the below defined function from Lec 3 slide 5
+# Step 1: Draw X ~ χ²(n - 1)
+draw_chi_sq <- function(n) {
+  return(rchisq(1, df = n - 1))
+}
+# Step 2: Compute σ² = (n - 1) * s² / X
+compute_sigma_sq <- function(n, segma2_0, X) {
+  return((n - 1) * segma2_0 / X)
+}
+
+# simulation
+segma_estimation <- function(n, mu_0, segma2_0, ndraws) {
+  results <- c()
+  
+  for (i in 1:ndraws) {
+    X <- draw_chi_sq(n)
+    sigma_sq <- compute_sigma_sq(n, segma2_0, X)
+    results[i] <- sigma_sq
+  }
+  return(results)
+}
+
+sigma2<-segma_estimation(n, mu_0, segma2_0, ndraws)
+
+#Now we estimate the betas values using the formula B|Segma2 follows N(muo,sigma2*omegao_inv) and we fit the regression based on 
+# temp = beta0 + beta1 time + beta 2 time^2 + erorr
+
+# first we have our error follows the normal distrbution by 0 and 1 
+
+for (i in 1:length(sigma2)) {
+  e<- rnorm(1,0,sigma2[i])
+  res<-rmvnorm(1,mu_0,sigma2[i]*omega_0)
+  temp= x=res[1,1]+res[1,2]*df$cov_tm+res[1,3]*df$cov_tm^2+e
+  df[[paste0("temp_p",i)]]<-temp 
+}
+
+plt<-ggplot(df,aes(x=cov_tm))+geom_line(aes(y=temp),
+                                        color='#FCA311', size=.5)+
+  geom_line(aes(y=temp_p), color='#14213D',linetype=1)+
+  #annotate(geom = "text", x = 8, y = Sd_true,
+  #         label = paste0(format(round(Sd_true, 3), nsmall = 3)))+
+  labs(x= 'time', y='temp')+
+  theme_classic()
+plt
+
+library(tidyverse)
+
+df_long <- df %>%
+  pivot_longer(cols = -x_column, names_to = "Line", values_to = "Value")
+
+detach("ggplot2", unload = TRUE)
